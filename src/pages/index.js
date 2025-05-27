ab78942518dc3df92b416d31c6db423f1d776523
CodeSandbox <support@codesandbox.io>
2025-05-27 14:12:32 +0000

    Remove pages directory and fix Next.js config

--- src/pages/index.js
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-  return (
-    <div className='min-h-screen bg-gray-100'>
-      <main className='container mx-auto px-4 py-8'>
-        <h1 className='text-4xl font-bold text-gray-900'>VPN Management System</h1>
-      </main>
-    </div>
-  )
-}
